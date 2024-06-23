841b321e41ce11c4492dbcdeb1e5149098ac399f
Tai_er <111734393+1316970594@users.noreply.github.com>
2024-06-23 17:11:34 +0800

    Update loss.py

--- models/loss.py
+++ models/loss.py
@@ -9,48 +9,6 @@ from .network_utils import backward_warp
 def tonemap(hdr_img, mu=5000):
     return (torch.log(1 + mu * hdr_img)) / math.log(1 + mu)
 
-class HDRFlow_Loss_2E(nn.Module):
-    def __init__(self, mu=5000):
-        super(HDRFlow_Loss_2E, self).__init__()
-        self.mu = mu
-
-    def forward(self, pred, hdrs, flow_preds, cur_ldr, flow_mask, flow_gts):
-        gt = hdrs[1]
-        mu_pred = tonemap(pred, self.mu)
-        mu_gt = tonemap(gt, self.mu)
-        recon_loss = nn.L1Loss()(mu_pred, mu_gt)
-        loss = recon_loss
-
-        Y = 0.299 * cur_ldr[:, 0] + 0.587 * cur_ldr[:, 1] + 0.114 * cur_ldr[:, 2]
-        Y = Y[:, None]
-        mask = (Y>0.8) | (Y<0.2)
-        mask = mask.repeat(1, 3, 1, 1)
-        p_flow, n_flow = flow_preds
-        if mask.sum() > 0:
-            p_warp_hdr = backward_warp(hdrs[0], p_flow)
-            n_warp_hdr = backward_warp(hdrs[2], n_flow)
-
-            mu_p_warp_hdr = tonemap(p_warp_hdr, self.mu)
-            mu_n_warp_hdr = tonemap(n_warp_hdr, self.mu)
-
-            p_align_loss = nn.L1Loss()(mu_p_warp_hdr[mask], mu_gt[mask])
-            n_align_loss = nn.L1Loss()(mu_n_warp_hdr[mask], mu_gt[mask])
-
-            hdr_align_loss = p_align_loss + n_align_loss
-            loss += 0.5 * hdr_align_loss
-
-        b, c, h, w = p_flow.shape       
-        flow_mask = flow_mask[:, None, None, None].repeat(1, 2, h, w)
-        flow_mask = flow_mask > 0.5        
-        if flow_mask.sum() > 0:
-            p_flow_loss = nn.L1Loss()(p_flow[flow_mask], flow_gts[0][flow_mask])
-            n_flow_loss = nn.L1Loss()(n_flow[flow_mask], flow_gts[1][flow_mask])
-
-            flow_loss = p_flow_loss + n_flow_loss
-            loss += 0.001 * flow_loss
-
-        return loss
-
 class HDRFlow_Loss_3E(nn.Module):
     def __init__(self, mu=5000):
         super(HDRFlow_Loss_3E, self).__init__()
